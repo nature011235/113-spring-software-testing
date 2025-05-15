37d8c2eff4a27b1aa0ed245692cbc2cc7080e17d
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,14 +2,5 @@
 
 void antiasan(unsigned long addr)
 {
-    extern char gS[];
-   
-    unsigned long gS_addr = (unsigned long)gS;
-    unsigned long shadow_addr = (gS_addr >> 3) + 0x7fff8000;
-    
-    unsigned char *shadow = (unsigned char *)shadow_addr;
 
-    for (int i = 0; i < 5; i++) {
-        shadow[i] = 0;  
-    }
 }
