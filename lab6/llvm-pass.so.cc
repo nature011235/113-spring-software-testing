3aea800276d5f9695e8cff03d45d4b05109f9356
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:47 +0800

    fix: lab8 no angr

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -13,52 +13,7 @@ PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
   FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
-  PointerType *CharPtrTy = Type::getInt8PtrTy(Ctx);
 
-
-  Function *MainFunc = M.getFunction("main");
-  if (!MainFunc) {
-    errs() << "Could not find main function\n";
-    return PreservedAnalyses::none();
-  }
-
-  BasicBlock &EntryBB = MainFunc->getEntryBlock();
-  IRBuilder<> Builder(&*EntryBB.begin());
-  
-
-  Builder.CreateCall(debug_func, {debug_arg});
-  
-
-  Constant *HayakuStr = ConstantDataArray::getString(Ctx, "hayaku... motohayaku!", true);
-  GlobalVariable *HayakuGV = new GlobalVariable(M, HayakuStr->getType(), true, 
-                                               GlobalValue::PrivateLinkage, HayakuStr, ".str");
-  
-
-  Constant *Zero = ConstantInt::get(Int32Ty, 0);
-  std::vector<Constant*> Indices = {Zero, Zero};
-  Constant *HayakuPtr = ConstantExpr::getGetElementPtr(HayakuStr->getType(), HayakuGV, Indices);
-  
-
-  Value *Argv = &*std::next(MainFunc->arg_begin()); 
-  Value *ArgvIdx = Builder.CreateGEP(CharPtrTy, Argv, Builder.getInt32(1));
-  Builder.CreateStore(HayakuPtr, ArgvIdx);
-  
-
-  AllocaInst *LocalArgc = Builder.CreateAlloca(Int32Ty, nullptr, "local_argc");
-  
-
-  Builder.CreateStore(debug_arg, LocalArgc);
-
-  Value *Argc = &*MainFunc->arg_begin(); 
-  SmallVector<User*, 8> Users(Argc->user_begin(), Argc->user_end());
-  
-  for (User *U : Users) {
-    if (Instruction *I = dyn_cast<Instruction>(U)) {
-      IRBuilder<> UseBuilder(I);
-      Value *LoadedArgc = UseBuilder.CreateLoad(Int32Ty, LocalArgc);
-      I->replaceUsesOfWith(Argc, LoadedArgc);
-    }
-  }
   for (auto &F : M) {
     errs() << "func: " << F.getName() << "\n";
 
